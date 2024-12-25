c2270104633d65fc6e68c4b4178ebb91cd08a0d0
ShivamGarg <shivamgarg1106@gmail.com>
2024-12-26 00:10:55 +0530

    modified:   src/3.graphs/02.lowest-common-ancestor.cpp 	modified:   src/3.graphs/05.bipartite-graph.cpp 	new file:   src/3.graphs/euler_path.cpp

--- src/3.graphs/02.lowest-common-ancestor.cpp
+++ src/3.graphs/02.lowest-common-ancestor.cpp
@@ -1,50 +1,52 @@
-int n, l; // l == logN (usually about ~20)
-vector<vector<int>> adj;
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
-
-void dfs(int v, int p)
+const int MOD = (int)1e9 + 7;
+const int LOG = ceil(log2(2e5 + 1));
+int gt = 0;
+vector<pair<int, int>> times(200001);
+vector<bool> visited(200001, false);
+vector<vector<int>> adj(200001);
+void dfs(int i, int p)
 {
-    tin[v] = ++timer;
-    up[v][0] = p;
-    // wUp[v][0] = weight[v][u]; // <- path weight sum to 2^i-th ancestor
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-        // wUp[v][i] = wUp[v][i-1] + wUp[up[v][i-1]][i-1];
+    visited[i] = true;
+    times[i].first = gt++;
+    for (auto it : adj[i])
+    {
+        if (it != p)
+        {
+            dfs(it, i);
+        }
+    }
+    times[i].second = gt++;
+}
+bool ancestor(int i, int j)
+{
+    return times[i].first <= times[j].first && times[i].second >= times[j].second;
+}
+signed main()
+{
 
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
+    vector<vector<int>> lifting(n + 1, vector<int>(LOG + 1));
+    for (int i = 2; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        adj[a].push_back(i);
+        lifting[i][0] = a;
+    }
+    lifting[1][0] = 1;
+    dfs(1, -1);
+    for (int i = 1; i <= LOG; i++)
+        for (int j = 1; j <= n; j++)
+            lifting[j][i] = lifting[lifting[j][i - 1]][i - 1];
+    // check if already ancestor otherwise
+    // for lca of a and b, // lifting[a][0] will be the final answer
+    for (int i = LOG; i >= 0; i--)
+    {
+        if (!ancestor(lifting[a][i], b))
+        {
+            a = lifting[a][i];
+        }
     }
 
-    tout[v] = ++timer;
-}
-
-bool isAncestor(int u, int v)
-{
-    return tin[u] <= tin[v] && tout[v] <= tout[u];
-}
-
-int lca(int u, int v)
-{
-    if (isAncestor(u, v))
-        return u;
-    if (isAncestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (!isAncestor(up[u][i], v))
-            u = up[u][i];
-    }
-    return up[u][0];
-}
-
-void preprocess(int root) {
-    tin.resize(n);
-    tout.resize(n);
-    timer = 0;
-    l = ceil(log2(n));
-    up.assign(n, vector<int>(l + 1));
-    dfs(root, root);
+    return 0;
 }
